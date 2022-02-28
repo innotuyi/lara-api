98edd3af5aab08eb8b87b4cdc2a15605b90dadfe
innotuyi <innotuyi67@gmail.com>
2022-02-28 16:47:30 +0200

    Adding sign in a user documentation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -129,6 +129,78 @@ class AuthController extends Controller
         return response()->json($response, 404);
     }
 
+/**
+     * Sign in a User
+     * @OA\Post (
+     *     path="/api/v1/user/signin",
+     *     tags={"user"},
+     *     security={{"bearer":{} }},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                      type="array",
+     *                       @OA\Items(
+     *                      @OA\Property(
+     *                          property="name",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="email",
+     *                          type="email"
+     *                      ),
+     *                     ),
+     *                 ),
+     *                 example={
+     *                     "name":"Innocent",
+    *                      "email":"innocent@gmail.com",
+     *                    }
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="name", type="number", example="innocent"),
+     *              @OA\Property(property="email", type="string", example="innocent@gmail.om"),
+     *              @OA\Property(property="password", type="string", example="innocent"),
+     * 
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="invalid",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="msg", type="string", example="fail"),
+     *          )
+     *      ),
+     *      @OA\Response(
+ *    response=401,
+ *    description="UnAuthorized",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="UnAuthanticated"),
+ *    )
+ * ),
+ *  @OA\Response(
+ *    response=500,
+ *    description="Returns when there is server problem",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="Server Error"),
+ *    )  
+ * ),
+ *  @OA\Response(
+     *          response=419,
+     *          description="CSRF Token mismatch",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="msg", type="string", example="fail"),
+     *          )
+     *      ),   
+     * )
+    */
+    
+
     public function signin(Request $request)
     {
         $this->validate($request, [
